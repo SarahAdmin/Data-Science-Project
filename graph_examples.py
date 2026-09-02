8e49565ab74b0eddb1ffc25880b174b43b200bf1
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2026-09-02 15:51:53 +0100

    Add additional plots to graph_examples.py
    Added histogram, KDE, and boxplot visualizations to graph_examples.py.

--- graph_examples.py
+++ graph_examples.py
@@ -5,3 +5,7 @@ import seaborn as sns
 sns.scatterplot(data=mydata,x='example1',y='example2')
 sns.barplot(data=mydata,x='example1',y='example2')  
 sns.lineplot(data=mydata,x='example1',y='example2') 
+sns.histplot(data=mydata,x='example1')
+sns.kdeplot(data=mydata,x='example1',fill=True,ci=None,fill=None) 
+sns.boxplot(data=mydata,x='example1')
+sns.boxplot(data=mydata,x='example1',y='example1')
