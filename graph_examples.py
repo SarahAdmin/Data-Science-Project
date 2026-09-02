8d9767b484e80e686739a3f2531add40974929ee
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2026-09-02 13:47:12 +0100

    Add barplot and lineplot to graph examples
    Added barplot and lineplot visualizations to graph_examples.py.

---
+++ graph_examples.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+sns.scatterplot(data=mydata,x='example1',y='example2')
+sns.barplot(data=mydata,x='example1',y='example2')  
+sns.lineplot(data=mydata,x='example1',y='example2') 
